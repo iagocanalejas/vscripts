f0428c505f8f262e192c3409248dbdc8406a31fa
Iagocanalejas <iagocanalejas@gmail.com>
2025-03-05 22:16:06 +0100

    feat: 'cap.XXX' name matcher & tests

--- tests/matcher_test.py
+++ tests/matcher_test.py
@@ -4,32 +4,25 @@ from vscripts.matcher import NameMatcher
 
 
 class TestNameMatcher(unittest.TestCase):
-    def test_matches_season_x_episode(self):
+    def test_classify(self):
         matcher = NameMatcher("Los Simpson - 2x04 - Dos Coches En Cada Garaje Y Tres Ojos En Cada Pez.avi")
-        self.assertTrue(matcher.matches_season_x_episode())
+        self.assertEqual(NameMatcher.Type.X_NAME, matcher.classify())
 
-    def test_matches_s_season_e_episode(self):
         matcher = NameMatcher("Los Simpson - S2E04 - Dos Coches En Cada Garaje Y Tres Ojos En Cada Pez.avi")
-        self.assertTrue(matcher.matches_s_season_e_episode())
+        self.assertEqual(NameMatcher.Type.SE_NAME, matcher.classify())
 
-    def test_matches_cap_dot_season_episode(self):
         matcher = NameMatcher("Los Simpson - Temporada 6 [DVDRIP][cap.601][Spanish][www.pctestrenos.com].avi")
-        self.assertTrue(matcher.matches_cap_dot_season_episode())
+        self.assertEqual(NameMatcher.Type.CAP_DOT_NAME, matcher.classify())
 
-    def test_clean_season_x_episode(self):
-        matcher = NameMatcher("Los Simpson - 2x04 - Dos Coches En Cada Garaje Y Tres Ojos En Cada Pez.avi")
-        self.assertEqual(
-            "Los Simpson - S2E04 - Dos Coches En Cada Garaje Y Tres Ojos En Cada Pez.mkv",
-            matcher.clean_season_x_episode(),
-        )
+    def test_clean(self):
+        matcher = NameMatcher("Los Simpson - 2x04 - Dos Coches En Cada.avi")
+        self.assertEqual("Los Simpson - S02E04 - Dos Coches En Cada.mkv", matcher.clean())
 
-    def test_clean_s_season_e_episode(self):
-        matcher = NameMatcher("Los Simpson - S2E04 - Dos Coches En Cada Garaje Y Tres Ojos En Cada Pez.avi")
-        self.assertEqual(
-            "Los Simpson - S2E04 - Dos Coches En Cada Garaje Y Tres Ojos En Cada Pez.mkv",
-            matcher.clean_s_season_e_episode(),
-        )
+        matcher = NameMatcher("9x07 - Las dos señoras Nahasapeemapetilon.avi")
+        self.assertEqual("S09E07 - Las dos señoras Nahasapeemapetilon.mkv", matcher.clean())
 
-    def test_clean_cap_dot_season_episode(self):
+        matcher = NameMatcher("Los Simpson - S2E04 - Dos Coches En Cada.avi")
+        self.assertEqual("Los Simpson - S02E04 - Dos Coches En Cada.mkv", matcher.clean())
+
         matcher = NameMatcher("Los Simpson - Temporada 6 [DVDRIP][cap.601][Spanish][www.pctestrenos.com].avi")
-        self.assertEqual("Los Simpson - S06E01.mkv", matcher.clean_cap_dot_season_episode())
+        self.assertEqual("Los Simpson - S06E01.mkv", matcher.clean())
